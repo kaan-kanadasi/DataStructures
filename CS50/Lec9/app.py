6731a6ac90a6c743e444709dad3eb609accd6389
kaan-kanadasi <kaankanadasi@gmail.com>
2024-10-05 19:07:35 +0000

    commit

--- CS50/Lec9/app.py
+++ CS50/Lec9/app.py
@@ -1,3 +1,5 @@
+# cd CS50    cd Lec9    flask run
+
 from flask import Flask, render_template, request
 
 # __name__ is a special variable that refers to the current file's name
@@ -5,4 +7,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "hello, world"
+    if "name" in request.args:
+        name = request.args["name"]
+    else:
+        name = "world"
+    return render_template("index.html", placeholder=name)
